4a37c083428907fc8f0e16db015f30c8f4416285
eggfly <lihaohua90@gmail.com>
2023-03-16 20:33:51 +0800

    boot.mpy and power_calc.py

--- power_calc.py
+++ power_calc.py
@@ -7,13 +7,15 @@ def calc_battery_life_in_days(battery_mah, wakeup_ma, deep_sleep_ua, wakeup_seco
     life_in_days = life_in_hours / 24.0
     return life_in_days
 
-print(calc_battery_life_in_days(120, 30.0, 76.8, 1.0, 59.0))
+# print(calc_battery_life_in_days(120, 30.0, 76.8, 1.0, 59.0))
 
-print(calc_battery_life_in_days(120, 11.0, 76.8, 1.0, 59.0))
+print('30mAh+MicroPython理论续航天使数')
+print(calc_battery_life_in_days(30, 20.0, 17.0, 0.399, 59.6))
 
-print(calc_battery_life_in_days(30, 19.0, 17.0, 0.1, 59.9))
+print('30mAh+Arduino理论续航天使数')
+print(calc_battery_life_in_days(30, 20.0, 17.0, 0.1, 59.9))
 
-print('开0秒，deep-sleep所有时间')
-print(calc_battery_life_in_days(120, 11.0, 20.0, 0.0, 59.0))
+# print('开0秒，deep-sleep所有时间')
+# print(calc_battery_life_in_days(120, 11.0, 20.0, 0.0, 59.0))
 
 
